9f8e4b7a9fb0151bb0764a21c2ecf99556bd1c1e
William Bryant <gogobebe2@gmail.com>
2021-07-07 03:17:47 +1200

    224

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:MyShop/providers/cart.dart';
 import 'package:MyShop/providers/orders.dart';
 import 'package:MyShop/screens/cart_screen.dart';
+import 'package:MyShop/screens/edit_product_screen.dart';
 import 'package:MyShop/screens/orders_screen.dart';
 import 'package:MyShop/screens/product_detail_screen.dart';
 import 'package:MyShop/screens/products_overview_screen.dart';
+import 'package:MyShop/screens/user_products_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -41,6 +43,8 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );
---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = "/edit-product";
+
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Product"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Title"),
+                  textInputAction: TextInputAction.next,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Price"),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Description"),
+                  maxLines: 3,
+                  keyboardType: TextInputType.multiline,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
